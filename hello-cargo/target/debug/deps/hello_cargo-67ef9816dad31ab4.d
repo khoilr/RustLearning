9d7b1a2a84d79a8ec5aad9a655a4ef3408873d8b
Lê Công Minh Khôi <lcngmnhkhoi@gmail.com>
2024-01-09 03:52:04 +0000

    hello world

---
+++ hello-cargo/target/debug/deps/hello_cargo-67ef9816dad31ab4.d
@@ -0,0 +1,5 @@
+/workspaces/RustLearning/hello-cargo/target/debug/deps/libhello_cargo-67ef9816dad31ab4.rmeta: src/main.rs
+
+/workspaces/RustLearning/hello-cargo/target/debug/deps/hello_cargo-67ef9816dad31ab4.d: src/main.rs
+
+src/main.rs:
---
+++ hello-cargo/target/debug/deps/hello_cargo-b09c5ecc1470bb9b.d
@@ -0,0 +1,5 @@
+/workspaces/RustLearning/hello-cargo/target/debug/deps/libhello_cargo-b09c5ecc1470bb9b.rmeta: src/main.rs
+
+/workspaces/RustLearning/hello-cargo/target/debug/deps/hello_cargo-b09c5ecc1470bb9b.d: src/main.rs
+
+src/main.rs:
